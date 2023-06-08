b64e6082a686ea66613614eae0b4f4ba51c79e0e
kaorimyzn <98532529+kaorimyzn@users.noreply.github.com>
2023-06-08 22:52:33 +0700

    TAMBAHJUDUL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PhotoGallery from './PhotoGalerry';
 function App() {
   return (
     <div>
+      <h1>Ujian Tengah Semester KPPAW 2105010005</h1>
       <Greeting />
       <FormValidation/>
       <PhotoGallery />
